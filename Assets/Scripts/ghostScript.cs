ff309d03f3a00f177649e715fb54f55a2668f3cc
matias_azucas@hotmail.com <matias_azucas@hotmail.com>
2020-01-09 15:33:47 -0300

    new ucaman

---
+++ Assets/Scripts/ghostScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class ghostScript : MonoBehaviour
+{
+    public GameObject target; //este es el player (jugador)
+    NavMeshAgent agent;//referencia para el ghost navmeshagent component
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if(target == null)
+        {
+            target = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //utilizado para actualizar la localizacion
+        agent.destination = target.transform.position;
+
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("ucaman_scene");
+        }
+    }
+}
