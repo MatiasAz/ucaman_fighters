923aade8750bc6c3cd1ab5a51ec2602035d0108a
matias_azucas@hotmail.com <matias_azucas@hotmail.com>
2020-01-16 15:21:29 -0300

    new

--- Assets/Scripts/ghostScript.cs
+++ Assets/Scripts/ghostScript.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class ghostScript : MonoBehaviour
 {
-    public GameObject target; //este es el player (jugador)
+    public GameObject target,target1,target2; //este es el player (jugador)
     NavMeshAgent agent;//referencia para el ghost navmeshagent component
 
     // Start is called before the first frame update
@@ -15,20 +15,30 @@ public class ghostScript : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         if(target == null)
         {
-            target = GameObject.FindGameObjectWithTag("Player");
+            target1 = GameObject.FindGameObjectWithTag("Player1");
+            target2 = GameObject.FindGameObjectWithTag("Player2");
+            target = target1;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if ((Vector3.Distance(agent.transform.position, target1.transform.position))> (Vector3.Distance(agent.transform.position, target2.transform.position)))
+        {
+            target = target2;
+        }
+        else
+        {
+            target = target1;
+        }
         //utilizado para actualizar la localizacion
         agent.destination = target.transform.position;
 
     }
     public void OnCollisionStay(Collision collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2")
         {
             //SceneManager.LoadScene("ucaman_scene");
         }
